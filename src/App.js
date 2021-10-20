f0a47f3ddb698349e877150d987145ef9288842c
sonalikhosla <sonalikhosla@gmail.com>
2021-10-19 23:02:15 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Css/App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExpandArrowsAlt, faCompressAlt } from '@fortawesome/free-solid-svg-icons';
 import { faFreeCodeCamp } from '@fortawesome/free-brands-svg-icons';
